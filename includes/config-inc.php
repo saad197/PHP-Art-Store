803c17b9acd9302b8dcce3d504c215f12d5a4693
Brijeshkumar Patel <b.patel405@mybvc.ca>
2018-03-30 12:10:35 -0600

    config file for database connection

---
+++ includes/config-inc.php
@@ -0,0 +1,7 @@
+<?php
+    define('DBHOST', 'local');
+    define('DBNAME', 'book');
+    define('DBUSER', 'test');
+    define('DBPASS', 'mypass');
+    define('DBCONNSTRING','mysql:dbname=book;charset=utf8mb4;');
+?>
