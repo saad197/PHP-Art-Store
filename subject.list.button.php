6999f7e7c5768abe760a20bb14bd090170c50d7d
sandeep saini <s.saini516@mybvc.ca>
2018-03-31 14:18:04 -0700

    add simple search page that filter the artists name

--- subject.list.button.php
+++ subject.list.button.php
@@ -3,20 +3,20 @@
 <html lang="en">
 <?php include("includes/head.inc.php"); ?>
 <body>
-<div class = "dropdown">
-<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Select subjectList
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-<?php 
-$subList = getSubjectList();
-foreach($subList as $listOfSubject){
-
-    echo '
-    <li>'.$listOfSubject->getSubjectID().' '.$listOfSubject->getSubjectName().'</li>';
-}
-?>
-</ul>
-</div>
+    <div class = "dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Select subjectList
+            <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <?php 
+                $subList = getSubjectList();
+                foreach($subList as $listOfSubject)
+                {
+                    echo '
+                    <li>'.$listOfSubject->getSubjectID().' '.$listOfSubject->getSubjectName().'</li>';
+                }
+            ?>
+        </ul>
+    </div>
 <?php include("includes/scripts.inc.php"); ?>
 </body>
 </html>
