ba0e517d6c3dd35aca0d22b7d07114adf504b079
k.agha809 <k.agha809@mybvc.ca>
2018-04-18 15:14:52 -0600

    sql for paintings table review counter

--- Sql/sql.sql
+++ Sql/sql.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `CustomerLogon` ADD `Admin` BOOLEAN NOT NULL AFTER `DateLastModified`;
 
-ALTER TABLE `Paintings` ADD `ReviewCount` INT NOT NULL AFTER `ReviewCount`;
+ALTER TABLE `Paintings` ADD `ReviewCount` INT NOT NULL AFTER `WikiLink`;
